1b138caebe097e7ee67ea13f8119d3483914582c
Ítalo Magno <italomagnov@gmail.com>
2022-10-20 14:07:53 -0400

    finalizando o curso de javascript para web

---
+++ main.js
@@ -0,0 +1,16 @@
+function tocaSom(idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);    
+    };
+
+}
